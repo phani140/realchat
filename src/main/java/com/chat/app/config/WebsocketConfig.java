85b1aaba471f5bbe0ea009b6d3863c2bd89996e9
phani140 <phaneendrareddys5@gmail.com>
2025-05-18 13:35:21 +0530

    first commit

---
+++ src/main/java/com/chat/app/config/WebsocketConfig.java
@@ -0,0 +1,28 @@
+package com.chat.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/chat")
+		        .setAllowedOrigins("http://localhost:1245")
+		        .withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		
+	 registry.enableSimpleBroker("/topic"); //topic //chat room,	
+	 registry.setApplicationDestinationPrefixes("/app");
+		
+	}
+
+	
+}
